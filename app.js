929d2d2d633d8a436e509441a69b6def866ddedc
Kostya-Nitro5 <Kostek26@rambler.ru>
2023-03-23 20:14:57 +0300

    add lesson 1

---
+++ app.js
@@ -0,0 +1,35 @@
+"Use strict";
+
+// Переменные и типы переменных
+let myName = 'Kostya',         /* string */
+    Age = 30,                  /* number */
+    Logic = true,              /* Bolean */
+    Pusto = null,              /* null */
+    Neopredeleno = undefined,  /* undefined */
+    Objject = {},              /* Object */
+    Symmbol =  Symbol(),       /* symbol */
+    notANumber = NaN;          /* NaN */
+
+    // myName = 'Nikname';
+// console.log(typeof(Pusto));
+
+// Операторы
+// let a = 5,
+//     b = 10,
+//     c;
+
+// c = Age + myName;
+// console.log(c);
+// унарный минус -с меняет знак на противоположный.
+// унарный плюс с приводит значение переменой к числу.
+
+// Всё true кроме: Null, undefined, 0, NaN, '';
+
+const aAge = 29; 
+if (aAge === 28 && age < 30) {
+    console.log('Возраст 28');
+}   else if (aAge < 28) {
+    console.log('Возраст меньше 28');
+}   else {
+    console.log('Возраст больше 28');
+}
